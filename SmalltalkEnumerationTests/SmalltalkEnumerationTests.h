bc80926770e890bb4cb9706938088f8eda1ea5f4
Olga <ozinchenko@hotmail.co.uk>
2013-02-28 08:29:27 +0000

    initial unit tests

---
+++ SmalltalkEnumerationTests/SmalltalkEnumerationTests.h
@@ -0,0 +1,13 @@
+//
+//  SmalltalkEnumerationTests.h
+//  SmalltalkEnumerationTests
+//
+//  Created by Olga Zinchenko on 28/02/2013.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SmalltalkEnumerationTests : SenTestCase
+
+@end
